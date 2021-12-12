6a1e380d7b9106ade1d817da6de814f3ebc35d38
Vladimir Kozlovsky <vovakozlouskiy@gmail.com>
2021-12-12 22:49:39 +0300

    Some improvements with account page

--- src/Components/Pages/AccountPage/style.js
+++ src/Components/Pages/AccountPage/style.js
@@ -5,17 +5,62 @@ const useStyles = makeStyles({
         flexDirection: "column",
         padding: "45px",
         width: "100%",
+        height: "100%",
+        boxSizing: "border-box",
     },
     accountInfoContainer: {
-        marginTop: "64px",
-        display:"flex",
-        flexDirection:"row",
-        columnGap:"24px",
-        alignItems:"center",
+        width: "300px",
+        display: "flex",
+        flexDirection: "column",
+        columnGap: "24px",
+        backgroundColor: "rgba(25, 118, 210, 0.07)",
+        borderRadius: "16px",
+        boxSizing: "border-box",
+        padding: "32px",
+        justifyContent: "space-between",
+    },
+    nameAndSurname: {
+        display: "flex",
+        flexDirection: "row",
+        marginTop: "48px",
+        columnGap:"16px",
     },
     avatarClass: {
         width: "128px",
         height: "128px",
+    },
+    accountMainContainer: {
+        display: "flex",
+        flexDirection: "row",
+        height: "100%",
+        marginTop: "45px",
+        columnGap: "12px",
+    },
+    mainAccountContainer: {
+        backgroundColor: "rgba(25, 118, 210, 0.07)",
+        flex: "1",
+        borderRadius: "16px",
+        padding: "32px",
+        display:"flex",
+        flexDirection:"column"
+    },
+    mainAccountContent:{
+        display:"flex",
+        flexDirection:"column",
+        rowGap:"12px",
+    },
+    topInfo: {
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+        rowGap: "28px",
+        marginTop: "48px",
+    },
+    bottomStatistics: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
     }
 });
 
