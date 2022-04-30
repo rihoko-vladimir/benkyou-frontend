c38f16e1a1f4f0cf7777836617cea0b2730a7cfd
rihoko <vovakozlouskiy@gmail.com>
2022-04-30 16:30:39 +0300

    Removed unnecessary sagas and replaced them with one generic saga.Also changed token refreshing behavior(now it will store tokens in the cookie instead of local storage)

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,23 +1,17 @@
 import axios from "axios";
 
-const baseUrl = "https://localhost:5001";
+const baseUrl = "http://localhost:5000";
 
-const config = (accessToken) => {
-    return {
-        headers: {
-            'Authorization': `Bearer ${accessToken}`
-        }
-    }
-}
+const config = {withCredentials: true}
 
 export const login = async ({login, password}) =>
-    await axios.post(`${baseUrl}/api/auth/login`, {login, password})
+    await axios.post(`${baseUrl}/api/auth/login`, {login, password}, config)
 
 export const register = async ({userName, email, firstName, lastName, password, isTermsAccepted}) =>
     await axios.post(`${baseUrl}/api/auth/register`, {userName, email, firstName, lastName, password, isTermsAccepted})
 
-export const refreshTokens = async ({refreshToken}) =>
-    await axios.post(`${baseUrl}/api/auth/refresh`, {refreshToken})
+export const refreshTokens = async () =>
+    await axios.post(`${baseUrl}/api/auth/refresh`,undefined, config)
 
 export const sendEmailCode = async ({emailCode, userId}) =>
     await axios.post(`${baseUrl}/api/auth/confirm-email`, {emailCode, userId})
@@ -28,38 +22,38 @@ export const resetPassword = async ({email}) =>
 export const resetPasswordConfirm = async ({password, email, token}) =>
     await axios.post(`${baseUrl}/api/auth/reset-password-confirm?email=${email}&token=${encodeURIComponent(token)}`, {password})
 
-export const getUserSets = async ({accessToken}) =>
-    await axios.get(`${baseUrl}/api/sets/my-collection`, config(accessToken))
+export const getUserSets = async () =>
+    await axios.get(`${baseUrl}/api/sets/my-collection`, config)
 
-export const createNewSet = async ({accessToken, name, description, kanjiList}) =>
-    await axios.put(`${baseUrl}/api/sets/create`, {name, description, kanjiList}, config(accessToken))
+export const createNewSet = async ({name, description, kanjiList}) =>
+    await axios.put(`${baseUrl}/api/sets/create`, {name, description, kanjiList}, config)
 
-export const removeSet = async ({accessToken, setId}) =>
-    await axios.delete(`${baseUrl}/api/sets/delete?setId=${setId}`, config(accessToken))
+export const removeSet = async ({setId}) =>
+    await axios.delete(`${baseUrl}/api/sets/delete?setId=${setId}`, config)
 
-export const modifySet = async ({accessToken, setId, name, description, kanjiList}) =>
-    await axios.put(`${baseUrl}/api/sets/modify`, {setId, name, description, kanjiList}, config(accessToken))
+export const modifySet = async ({setId, name, description, kanjiList}) =>
+    await axios.put(`${baseUrl}/api/sets/modify`, {setId, name, description, kanjiList}, config)
 
-export const getAllSets = async ({pageNumber, pageSize, accessToken}) =>
-    await axios.get(`${baseUrl}/api/sets/all?page=${pageNumber}&size=${pageSize}`, config(accessToken))
+export const getAllSets = async ({pageNumber, pageSize}) =>
+    await axios.get(`${baseUrl}/api/sets/all?page=${pageNumber}&size=${pageSize}`, config)
 
-export const getAllSetsByQuery = async ({pageNumber, pageSize, query, accessToken}) =>
-    await axios.get(`${baseUrl}/api/sets/all_search?page=${pageNumber}&size=${pageSize}&name=${query}`, config(accessToken))
+export const getAllSetsByQuery = async ({pageNumber, pageSize, query}) =>
+    await axios.get(`${baseUrl}/api/sets/all_search?page=${pageNumber}&size=${pageSize}&name=${query}`, config)
 
-export const changeVisibility = async ({accessToken, isPublic}) =>
-    await axios.patch(`${baseUrl}/api/account/change-visibility?isPublic=${isPublic}`,undefined, config(accessToken))
+export const changeVisibility = async ({isPublic}) =>
+    await axios.patch(`${baseUrl}/api/account/change-visibility?isPublic=${isPublic}`,undefined, config)
 
-export const getAccountInfo = async ({accessToken}) =>
-    await axios.get(`${baseUrl}/api/account/`, config(accessToken))
+export const getAccountInfo = async () =>
+    await axios.get(`${baseUrl}/api/account/`, config)
 
-export const updateUserInfo = async ({accessToken, firstName, lastName, birthday, about, avatar}) =>
+export const updateUserInfo = async ({firstName, lastName, birthday, about, avatar}) =>
     await axios.put(`${baseUrl}/api/account/modify`, {
         firstName,
         lastName,
         birthday,
         about,
         avatar
-    }, config(accessToken))
+    }, config)
 
 export const checkEmail = async ({email}) =>
     await axios.get(`${baseUrl}/api/auth/check-email?email=${email}`);
